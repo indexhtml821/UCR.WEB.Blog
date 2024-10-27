4b81bcc80279a1cc7231647f27568f9e3bb2ed7a
indexhtml821 <66281437+indexhtml821@users.noreply.github.com>
2024-10-26 23:08:14 -0600

    Agregar archivos de proyecto.

---
+++ UCR.WEB.Blog/Models/Data/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+namespace UCR.WEB.Blog.Models.Data;
+
+
+public class BlogDbContext : DbContext
+{
+    public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
+
---
+++ UCR.WEB.Blog/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace UCR.WEB.Blog.Models;
+
+public class Post
+{
+    public Post() { Comments = new List<Comment>(); }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public ICollection<Comment> Comments { get; set; }
+}
