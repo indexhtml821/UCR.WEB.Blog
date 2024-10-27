1eaa9eb447197b3bdd99f6883c0b149173d5ea42
indexhtml821 <66281437+indexhtml821@users.noreply.github.com>
2024-10-27 12:45:50 -0600

    adds posts view

--- UCR.WEB.Blog/Controllers/PostsController.cs
+++ UCR.WEB.Blog/Controllers/PostsController.cs
@@ -3,91 +3,91 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UCR.WEB.Blog.Models.Data;
 
-namespace UCR.WEB.Blog.Controllers
+namespace UCR.WEB.Blog.Controllers;
+
+public class PostsController : Controller
 {
-    public class PostsController : Controller
+    private readonly BlogDbContext _context;
+
+    public PostsController(BlogDbContext context)
     {
-        private readonly BlogDbContext _context;
+        _context = context;
+    }
 
-        public PostsController(BlogDbContext context)
+
+    // GET: PostsController
+    public async Task<IActionResult> Index()
+    {
+
+        return View(await _context.Posts.ToListAsync());
+    }
+
+    // GET: PostsController/Details/5
+    public ActionResult Details(int id)
+    {
+        return View();
+    }
+
+    // GET: PostsController/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: PostsController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+        try
         {
-            _context = context;
+            return RedirectToAction(nameof(Index));
         }
-
-
-        // GET: PostsController
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Posts.ToListAsync());
-        }
-
-        // GET: PostsController/Details/5
-        public ActionResult Details(int id)
+        catch
         {
             return View();
         }
+    }
 
-        // GET: PostsController/Create
-        public ActionResult Create()
+    // GET: PostsController/Edit/5
+    public ActionResult Edit(int id)
+    {
+        return View();
+    }
+
+    // POST: PostsController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
         {
             return View();
         }
+    }
 
-        // POST: PostsController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+    // GET: PostsController/Delete/5
+    public ActionResult Delete(int id)
+    {
+        return View();
+    }
+
+    // POST: PostsController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        try
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction(nameof(Index));
         }
-
-        // GET: PostsController/Edit/5
-        public ActionResult Edit(int id)
+        catch
         {
             return View();
-        }
-
-        // POST: PostsController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: PostsController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: PostsController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
