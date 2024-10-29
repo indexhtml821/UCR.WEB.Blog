9f40d8d174fb6e32cd06e4ffcae48503446a9cd1
Christopher Duran <0xchris99@gmail.coom>
2024-10-29 14:28:40 -0600

    Rerun automatic generation of code

--- UCR.WEB.Blog/Controllers/PostsController.cs
+++ UCR.WEB.Blog/Controllers/PostsController.cs
@@ -1,93 +1,157 @@
-﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using UCR.WEB.Blog.Models;
 using UCR.WEB.Blog.Models.Data;
 
-namespace UCR.WEB.Blog.Controllers;
-
-public class PostsController : Controller
+namespace UCR.WEB.Blog.Controllers
 {
-    private readonly BlogDbContext _context;
-
-    public PostsController(BlogDbContext context)
+    public class PostsController : Controller
     {
-        _context = context;
-    }
+        private readonly BlogDbContext _context;
 
-
-    // GET: PostsController
-    public async Task<IActionResult> Index()
-    {
-
-        return View(await _context.Posts.ToListAsync());
-    }
-
-    // GET: PostsController/Details/5
-    public ActionResult Details(int id)
-    {
-        return View();
-    }
-
-    // GET: PostsController/Create
-    public ActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: PostsController/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create(IFormCollection collection)
-    {
-        try
+        public PostsController(BlogDbContext context)
         {
-            return RedirectToAction(nameof(Index));
+            _context = context;
         }
-        catch
+
+        // GET: Posts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Posts.ToListAsync());
+        }
+
+        // GET: Posts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public IActionResult Create()
         {
             return View();
         }
-    }
 
-    // GET: PostsController/Edit/5
-    public ActionResult Edit(int id)
-    {
-        return View();
-    }
-
-    // POST: PostsController/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, IFormCollection collection)
-    {
-        try
+        // POST: Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Body")] Post post)
         {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Body")] Post post)
+        {
+            if (id != post.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+            }
+
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-        catch
-        {
-            return View();
-        }
-    }
 
-    // GET: PostsController/Delete/5
-    public ActionResult Delete(int id)
-    {
-        return View();
-    }
-
-    // POST: PostsController/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-        try
+        private bool PostExists(int id)
         {
-            return RedirectToAction(nameof(Index));
-        }
-        catch
-        {
-            return View();
+            return _context.Posts.Any(e => e.Id == id);
         }
     }
 }
