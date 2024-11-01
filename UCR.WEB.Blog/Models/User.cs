27e390fa2b4808c8a70860099fb67dd9391a0456
indexhtml821 <isaacopine@gmail.com>
2024-10-31 23:53:40 -0600

    defines needed attributes for user using identity

--- UCR.WEB.Blog/Models/User.cs
+++ UCR.WEB.Blog/Models/User.cs
@@ -4,10 +4,8 @@ namespace UCR.WEB.Blog.Models;
 
 public class User:IdentityUser
 {
-    public int Id { get; set; }
     public string Name { get; set; }
-    public string Email { get; set; }
     public string Role { get; set; }
-
     public string Password { get; set; }
+
 }
