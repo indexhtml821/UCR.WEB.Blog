bf7ad2e732d1a353baf8483b4246fac4ae236ae7
indexhtml821 <66281437+indexhtml821@users.noreply.github.com>
2024-11-07 22:13:54 -0600

    deletes non needed role space

--- UCR.WEB.Blog/Models/User.cs
+++ UCR.WEB.Blog/Models/User.cs
@@ -5,7 +5,7 @@ namespace UCR.WEB.Blog.Models;
 public class User:IdentityUser
 {
     public string Name { get; set; }
-  //  public string Role { get; set; }
+    public string? Role { get; set; }
     public string Password { get; set; }
 
 }
